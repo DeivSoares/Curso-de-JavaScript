9eabc96445cc07625e4c744f24bc0a3a9986012d
Deivison Soares <deivisonsoares1610@outlook.com>
2022-06-20 06:42:42 -0300

    .

---
+++ aula001/script.js
@@ -0,0 +1,9 @@
+let nome = 'Deivison'
+let idade = 22;
+let profissao = 'Desenvolvedor Web'
+let altura = 1.70;
+
+console.log(`Me chamo ${nome}`);
+console.log(`Tenho ${idade} anos`);
+console.log(`Tenho ${altura}0cm`);
+console.log(`Eu sou um ${profissao}`);
---
+++ aula001/script3.js
@@ -0,0 +1,5 @@
+let nome = 'Deivion'; //String Literal
+let idade = 25; //Number Literal
+let estaAprovado = true; //Boolean
+let sobrenome = undefined; //Undefined
+let corSelecionado = null; //Redefinir um valor
